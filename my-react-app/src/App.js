77fb1e173c50de121e089938be777343ba2472bb
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-09-01 16:45:45 -0400

    added axios calls

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div>
-      hey
-    </div>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {},
+      userFollowers: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users/cory-thomas')
+      .then(response => {
+        console.log(response.data)
+        this.setState({ user: response.data });
+        axios.get("https://api.github.com/users/Cory-Thomas/followers")
+          .then(response => {
+            console.log(response.data)
+            response.data.forEach(follower => {
+              this.setState({
+                      userFollowers: response.data
+                    })
+            })
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        hey
+        {/* <RootUser user={this.state.user} />
+        <FollowerList followers={this.state.followers} /> */}
+      </div>
+    );
+  }
 }
-
-export default App;
